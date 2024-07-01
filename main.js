e1c4724d1e1a4dc447047c9f8cdeb6acc199e612
Leoyan <71907948+Risole490@users.noreply.github.com>
2024-07-01 20:47:31 -0300

    Dark mode no header em processo

--- main.js
+++ main.js
@@ -11,10 +11,33 @@ document.getElementById('checkbox').addEventListener('change', function() {
     // Opcional: Salvar a preferência do usuário
     const isDarkMode = document.body.classList.contains('dark-mode');
     localStorage.setItem('darkMode', this.checked ? 'enabled' : 'disabled');
+
+    if (isDarkMode) {
+      document.getElementById('switch-sun').src = '/imgs/sun.png';
+      document.getElementById('switch-moon').src = '/imgs/moon.png';
+      document.getElementById('menu-icon').src = '/imgs/menu-icon-dark.png';
+    } else {
+      document.getElementById('switch-sun').src = '/imgs/sunlight.png';
+      document.getElementById('switch-moon').src = '/imgs/moonlight.png';
+      document.getElementById('menu-icon').src = '/imgs/menu-icon.png';
+    }
+
   });
   
   // Opcional: Verificar a preferência do usuário ao carregar a página
   if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
     document.getElementById('checkbox').checked = true;
+
+    const isDarkMode = document.body.classList.contains('dark-mode');
+
+    if (isDarkMode) {
+      document.getElementById('switch-sun').src = '/imgs/sun.png';
+      document.getElementById('switch-moon').src = '/imgs/moon.png';
+      document.getElementById('menu-icon').src = '/imgs/menu-icon-dark.png';
+    } else {
+      document.getElementById('switch-sun').src = '/imgs/sunlight.png';
+      document.getElementById('switch-moon').src = '/imgs/moonlight.png';
+      document.getElementById('menu-icon').src = '/imgs/menu-icon.png';
+    }
   }
