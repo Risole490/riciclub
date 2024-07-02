8be1897df4567a1049e5c3b3bcf9845224ef602b
Leoyan <71907948+Risole490@users.noreply.github.com>
2024-07-02 10:41:23 -0300

    Dark Mode applied succesfully in header

--- main.js
+++ main.js
@@ -4,40 +4,67 @@ logoMenuMobile.addEventListener('click', () => {
     document.getElementById('headermenumobile-menu_lista').classList.toggle('hidden');
 });
 
-// JavaScript
-document.getElementById('checkbox').addEventListener('change', function() {
-    document.body.classList.toggle('dark-mode');
-    
-    // Opcional: Salvar a preferência do usuário
-    const isDarkMode = document.body.classList.contains('dark-mode');
-    localStorage.setItem('darkMode', this.checked ? 'enabled' : 'disabled');
+// Lógicas para alternar o tema claro/escuro
 
-    if (isDarkMode) {
-      document.getElementById('switch-sun').src = '/imgs/sun.png';
-      document.getElementById('switch-moon').src = '/imgs/moon.png';
-      document.getElementById('menu-icon').src = '/imgs/menu-icon-dark.png';
-    } else {
-      document.getElementById('switch-sun').src = '/imgs/sunlight.png';
-      document.getElementById('switch-moon').src = '/imgs/moonlight.png';
-      document.getElementById('menu-icon').src = '/imgs/menu-icon.png';
-    }
 
-  });
+function atualizaTema() {
+  const isDarkMode = document.getElementById('checkbox').checked;
   
-  // Opcional: Verificar a preferência do usuário ao carregar a página
-  if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-    document.getElementById('checkbox').checked = true;
+  // Define os caminhos das imagens de acordo com o tema
+  const imagePaths = {
+    'switch-sun': isDarkMode ? '/imgs/sun.png' : '/imgs/sunlight.png', 
+    'switch-moon': isDarkMode ? '/imgs/moon.png' : '/imgs/moonlight.png',
+    'menu-icon': isDarkMode ? '/imgs/menu-icon-dark.png' : '/imgs/menu-icon.png',
+  };
 
-    const isDarkMode = document.body.classList.contains('dark-mode');
-
-    if (isDarkMode) {
-      document.getElementById('switch-sun').src = '/imgs/sun.png';
-      document.getElementById('switch-moon').src = '/imgs/moon.png';
-      document.getElementById('menu-icon').src = '/imgs/menu-icon-dark.png';
-    } else {
-      document.getElementById('switch-sun').src = '/imgs/sunlight.png';
-      document.getElementById('switch-moon').src = '/imgs/moonlight.png';
-      document.getElementById('menu-icon').src = '/imgs/menu-icon.png';
+  // Atualiza as imagens de acordo com o tema
+  Object.entries(imagePaths).forEach(([id, src]) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.src = src;
     }
-  }
+  });
+
+  // Atualiza a cor dos fundos
+  alteraCorDeFundo(isDarkMode);
+
+  // Atualiza a cor dos textos
+  alteraCorDoTexto(isDarkMode);
+
+}
+
+function alteraCorDeFundo(isDarkMode) {
+  const corDeFundo = isDarkMode ? '#000' : '#FFF';
+
+  document.getElementById('header_container').style.backgroundColor = corDeFundo;
+  document.getElementById('headermenumobile-menu_lista').style.backgroundColor = corDeFundo;
+}
+
+function alteraCorDoTexto(isDarkMode) {
+  const corDoTexto = isDarkMode ? '#FFF' : '#000';
+
+  document.body.style.color = corDoTexto
+  document.querySelectorAll('#mobile-menu-item_text').forEach(item => {
+    item.style.color = corDoTexto;
+  });
+}
+
+// Adiciona o listener ao checkbox
+document.getElementById('checkbox').addEventListener('change', function() {
+  
+  // Opcional: Salvar a preferência do usuário
+  localStorage.setItem('darkMode', this.checked ? 'enabled' : 'disabled');
+  
+  // Atualiza o tema da página
+  atualizaTema();
+});
+
+// Verifica a preferência do usuário ao carregar a página
+if (localStorage.getItem('darkMode') === 'enabled') {
+  document.getElementById('checkbox').checked = true;
+  document.getElementById('header_container').classList.add('darkmode-bg');
+
+}
+
+// Atualiza o tema da página ao carregar
+atualizaTema();
